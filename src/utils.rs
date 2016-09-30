00b708a2996dc733a06b5356e66f47dcec6e236d
Adam Ransom <v0id.gmr@gmail.com>
2016-09-30 07:55:19 +0000

    Add tests for synchsafe conversion

--- src/utils.rs
+++ src/utils.rs
@@ -8,12 +8,12 @@
 /// # use id3::utils::synchsafe_to_u32;
 /// let synchsafe: [u8; 4] = [
 ///     0b0000_0000,
-///     0b0000_0000,
+///     0b0000_0011,
 ///     0b0111_1111,
 ///     0b0111_1111,
 /// ];
 /// let integer = synchsafe_to_u32(&synchsafe);
-/// assert_eq!(integer, Some(0b00111111_11111111));
+/// assert_eq!(integer, Some(0b11111111_11111111));
 /// ```
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub fn synchsafe_to_u32(buf: &[u8]) -> Option<u32> {
@@ -26,5 +26,32 @@ pub fn synchsafe_to_u32(buf: &[u8]) -> Option<u32> {
         )
     } else {
         None
+    }
+}
+
+#[cfg(test)]
+mod synchsafe_to_u32_tests {
+    use super::synchsafe_to_u32;
+
+    fn assert_eq(bytes: &[u8], expected: u32) {
+        let actual = synchsafe_to_u32(bytes);
+        assert_eq!(actual, Some(expected));
+    }
+
+    #[test]
+    fn it_converts() {
+        assert_eq(&[0, 0, 0, 0], 0);
+        assert_eq(&[0, 0, 0, 0x1], 0x1);
+        assert_eq(&[0, 0, 0, 0x7F], 0x7F);
+        assert_eq(&[0, 0, 0x1, 0], 0x80);
+        assert_eq(&[0, 0, 0x1, 0x7F], 0xFF);
+        assert_eq(&[0, 0x3, 0x7F, 0x7F], 0xFFFF);
+        assert_eq(&[0x7, 0x7F, 0x7F, 0x7F], 0xFFFFFF);
+        assert_eq(&[0x7F, 0x7F, 0x7F, 0x7F], 0xFFFFFFF);
+    }
+
+    #[test]
+    fn it_returns_none() {
+        assert_eq!(synchsafe_to_u32(&[]), None);
     }
 }
